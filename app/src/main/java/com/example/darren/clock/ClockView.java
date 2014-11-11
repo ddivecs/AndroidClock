9fb6d15528f612a026e8c768e36048bb37235e8a
Matt Rzepka <matthewrzepka@gmail.com>
2014-11-11 13:32:15 -0500

    Now has 2 paddles and ball on main activity, need to adjust height of paddles and movement of paddles and ball throughout clock in reference to current time. Also need to work out math with 12 vs 24 hour clock

--- app/src/main/java/com/example/darren/clock/ClockView.java
+++ app/src/main/java/com/example/darren/clock/ClockView.java
@@ -13,6 +13,8 @@ import android.view.View;
 public class ClockView extends View{
 
     private Paint _clockPaint = new Paint();
+    private int leftPaddleTop, leftPaddleHeight, rightPaddleTop, rightPaddleHeight, paddleWidth,
+        ballWidth, ballHeight, ballTop, ballLeft, numHours, numMins, numSecs;
 
     public ClockView(Context context) {
         super(context);
@@ -30,6 +32,26 @@ public class ClockView extends View{
     }
 
     private void init(AttributeSet attrs, int defStyleAttr){
+        numHours = 12;
+        numMins = 60;
+        numSecs = 60;
+
+        //paddle settings
+        leftPaddleTop = 0;
+        leftPaddleHeight = 100;
+
+        rightPaddleTop = 0;
+        rightPaddleHeight = 100;
+
+        paddleWidth = 10;
+
+        //ball settings
+        ballLeft = paddleWidth;
+        ballTop = 0;
+        ballWidth = 15;
+        ballHeight = 15;
+
+        //paint settings
         _clockPaint.setColor(Color.RED);
         _clockPaint.setAntiAlias(true);
     }
@@ -38,7 +60,10 @@ public class ClockView extends View{
     public void onDraw(Canvas canvas){
         super.onDraw(canvas);
 
-        canvas.drawRect(0, 0, 100, 100, _clockPaint);
+        //draw the three rectangles
+        canvas.drawRect(0, leftPaddleTop, paddleWidth, leftPaddleTop + leftPaddleHeight, _clockPaint);
+        canvas.drawRect(getWidth()-paddleWidth, rightPaddleTop, getWidth(), rightPaddleTop + rightPaddleHeight, _clockPaint);
+        canvas.drawRect(ballLeft, ballTop, ballLeft + ballWidth, ballTop + ballHeight, _clockPaint);
 
     }
 }
